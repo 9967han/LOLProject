8f2b1f2c51cfc4c0e01ab95d2721ff0b1145ce82
username <andstu1231@gmail.com>
2021-09-24 05:26:34 +0900

    api 코드 수정

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// nodemon app.js로 서버 실행
 var express = require("express");
 var axios = require("axios");
 var urlencode = require("urlencode");
@@ -25,31 +26,31 @@ app.use(express.static("public"));
 app.get("/search", async (req, res) => {
   console.log("id2 : " + req.query.id2);
   res.sendFile(__dirname + "/public/form.html");
-  const summoner1 = await axios.get(
+  const { data: summoner1 } = await axios.get(
     "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
       urlencode(req.query.id1) +
       "?api_key=" +
       api_key
   );
-  const summoner2 = await axios.get(
+  const { data: summoner2 } = await axios.get(
     "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
       urlencode(req.query.id2) +
       "?api_key=" +
       api_key
   );
-  const summoner3 = await axios.get(
+  const { data: summoner3 } = await axios.get(
     "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
       urlencode(req.query.id3) +
       "?api_key=" +
       api_key
   );
-  const summoner4 = await axios.get(
+  const { data: summoner4 } = await axios.get(
     "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
       urlencode(req.query.id4) +
       "?api_key=" +
       api_key
   );
-  const summoner5 = await axios.get(
+  const { data: summoner5 } = await axios.get(
     "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
       urlencode(req.query.id5) +
       "?api_key=" +
@@ -57,65 +58,65 @@ app.get("/search", async (req, res) => {
   );
   const summoner1ChampionMastery = await axios.get(
     "https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" +
-      summoner1.data.id +
+      summoner1.id +
       "?api_key=" +
       api_key
   );
   const summoner2ChampionMastery = await axios.get(
     "https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" +
-      summoner2.data.id +
+      summoner2.id +
       "?api_key=" +
       api_key
   );
   const summoner3ChampionMastery = await axios.get(
     "https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" +
-      summoner3.data.id +
+      summoner3.id +
       "?api_key=" +
       api_key
   );
   const summoner4championMastery = await axios.get(
     "https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" +
-      summoner4.data.id +
+      summoner4.id +
       "?api_key=" +
       api_key
   );
   const summoner5ChampionMastery = await axios.get(
     "https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" +
-      summoner5.data.id +
+      summoner5.id +
       "?api_key=" +
       api_key
   );
-  const summoner1MatchList = await axios.get(
+  const { data: summoner1MatchList } = await axios.get(
     "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" +
-      summoner1.data.puuid +
+      summoner1.puuid +
       "/ids" +
       "?api_key=" +
       api_key
   );
-  const summoner2MatchList = await axios.get(
+  const { data: summoner2MatchList } = await axios.get(
     "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" +
-      summoner2.data.puuid +
+      summoner2.puuid +
       "/ids" +
       "?api_key=" +
       api_key
   );
-  const summoner3MatchList = await axios.get(
+  const { data: summoner3MatchList } = await axios.get(
     "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" +
-      summoner3.data.puuid +
+      summoner3.puuid +
       "/ids" +
       "?api_key=" +
       api_key
   );
-  const summoner4MatchList = await axios.get(
+  const { data: summoner4MatchList } = await axios.get(
     "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" +
-      summoner4.data.puuid +
+      summoner4.puuid +
       "/ids" +
       "?api_key=" +
       api_key
   );
-  const summoner5MatchList = await axios.get(
+  const { data: summoner5MatchList } = await axios.get(
     "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" +
-      summoner5.data.puuid +
+      summoner5.puuid +
       "/ids" +
       "?api_key=" +
       api_key
