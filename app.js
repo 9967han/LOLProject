814d443e4eda7bf2cfda401cb74ff6bd5e96d6a3
sangwook <9967han@naver.com>
2021-10-20 17:11:52 +0900

    박찬순/summoner data 가져오기

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 const api_key = ""; // 여기에 key 입력
 
 var app = express(); // express 함수 실행
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.listen(3000, function () {
   // 3000포트로 실행하고 대기, 비동기로 인해 나중에 실행됨
@@ -30,11 +30,11 @@ app.get("/search", async (req, res) => {
   // console.log("id2 : " + req.query.id2);
   // res.sendFile(__dirname + "/public/form.html");
   const sleep = (ms) => {
-    return new Promise(resolve=>{
-        setTimeout(resolve,ms)
-    })
-  }
-  try{ 
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+  try {
     const { data: summoner1 } = await axios.get(
       "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
         urlencode(req.query.id1) +
@@ -110,6 +110,7 @@ app.get("/search", async (req, res) => {
         "&api_key=" +
         api_key
     );
+
     const { data: summoner3MatchList } = await axios.get(
       "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" +
         urlencode(summoner3.puuid) +
@@ -132,72 +133,89 @@ app.get("/search", async (req, res) => {
         api_key
     );
     await sleep(1000);
-    const summoner1Match = [], summoner2Match = [], summoner3Match = [], summoner4Match = [], summoner5Match = [];
+    const summoner1Match = [],
+      summoner2Match = [],
+      summoner3Match = [],
+      summoner4Match = [],
+      summoner5Match = [];
     async function makeSummonerMatch() {
       for (const matchId of summoner1MatchList) {
-        let result = await axios.get(
+        let { data: result } = await axios.get(
           "https://asia.api.riotgames.com/lol/match/v5/matches/" +
             urlencode(matchId) +
             "?api_key=" +
             api_key
         );
-        summoner1Match.push(result);
+        if (result.info.queueId == 420 || result.info.queueId == 440) {
+          result = result.info.participants;
+          summoner1Match.push(result);
+        }
       }
       await sleep(1000);
       for (const matchId of summoner2MatchList) {
-        let result = await axios.get(
+        let { data: result } = await axios.get(
           "https://asia.api.riotgames.com/lol/match/v5/matches/" +
             urlencode(matchId) +
             "?api_key=" +
             api_key
         );
-        summoner2Match.push(result);
+        if (result.info.queueId == 420 || result.info.queueId == 440) {
+          result = result.info.participants;
+          summoner2Match.push(result);
+        }
       }
-      await sleep(1000);
+      await sleep(2000);
       for (const matchId of summoner3MatchList) {
-        let result = await axios.get(
+        let { data: result } = await axios.get(
           "https://asia.api.riotgames.com/lol/match/v5/matches/" +
             urlencode(matchId) +
             "?api_key=" +
             api_key
         );
-        summoner3Match.push(result);
+        if (result.info.queueId == 420 || result.info.queueId == 440) {
+          result = result.info.participants;
+          summoner3Match.push(result);
+        }
       }
       await sleep(1000);
       for (const matchId of summoner4MatchList) {
-        let result = await axios.get(
+        let { data: result } = await axios.get(
           "https://asia.api.riotgames.com/lol/match/v5/matches/" +
             urlencode(matchId) +
             "?api_key=" +
             api_key
         );
-        summoner4Match.push(result);
+        if (result.info.queueId == 420 || result.info.queueId == 440) {
+          result = result.info.participants;
+          summoner4Match.push(result);
+        }
       }
       await sleep(1000);
       for (const matchId of summoner5MatchList) {
-        let result = await axios.get(
+        let { data: result } = await axios.get(
           "https://asia.api.riotgames.com/lol/match/v5/matches/" +
             urlencode(matchId) +
             "?api_key=" +
             api_key
         );
-        summoner5Match.push(result);
+        if (result.info.queueId == 420 || result.info.queueId == 440) {
+          result = result.info.participants;
+          summoner5Match.push(result);
+        }
       }
     }
     await makeSummonerMatch();
-    console.log(summoner1Match, summoner2Match, summoner3Match, summoner4Match, summoner5Match);
-
     const data = {
-      id1 : req.query.id1,
-      id2 : req.query.id2,
-      id3 : req.query.id3,
-      id4 : req.query.id4,
-      id5 : req.query.id5,
-      result : 74
+      id1: req.query.id1,
+      id2: req.query.id2,
+      id3: req.query.id3,
+      id4: req.query.id4,
+      id5: req.query.id5,
+      result: 74,
     };
-    res.render('form.ejs', data);
-  } catch(error){
+    res.render("form.ejs", data);
+
+  } catch (error) {
     console.log(error);
   }
-
 });
