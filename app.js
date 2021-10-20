58678dbfb81ce02322068eacde59499f043ec2c8
sangwook <9967han@naver.com>
2021-10-21 01:32:20 +0900

    한상욱 유재훈/python 연동, data parsing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var axios = require("axios");
 var urlencode = require("urlencode");
+var PythonShell = require('python-shell');
 const ejs = require("ejs");
 const api_key = ""; // 여기에 key 입력
 
@@ -64,7 +65,7 @@ app.get("/search", async (req, res) => {
         urlencode(req.query.id5) +
         "?api_key=" +
         api_key
-    );
+    );/*
     const summoner1ChampionMastery = await axios.get(
       "https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" +
         urlencode(summoner1.id) +
@@ -94,7 +95,7 @@ app.get("/search", async (req, res) => {
         urlencode(summoner5.id) +
         "?api_key=" +
         api_key
-    );
+    );*/
     await sleep(1000);
     const { data: summoner1MatchList } = await axios.get(
       "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" +
@@ -133,11 +134,13 @@ app.get("/search", async (req, res) => {
         api_key
     );
     await sleep(1000);
-    const summoner1Match = [],
-      summoner2Match = [],
-      summoner3Match = [],
-      summoner4Match = [],
-      summoner5Match = [];
+    const summoner1Match = [], summoner2Match = [], summoner3Match = [], summoner4Match = [], summoner5Match = [];
+    const player1Data = Array.from({length:16}, ()=>0);
+    const player2Data = Array.from({length:16}, ()=>0);
+    const player3Data = Array.from({length:16}, ()=>0);
+    const player4Data = Array.from({length:16}, ()=>0);
+    const player5Data = Array.from({length:16}, ()=>0);
+
     async function makeSummonerMatch() {
       for (const matchId of summoner1MatchList) {
         let { data: result } = await axios.get(
@@ -147,7 +150,8 @@ app.get("/search", async (req, res) => {
             api_key
         );
         if (result.info.queueId == 420 || result.info.queueId == 440) {
-          result = result.info.participants;
+          resultIdx = result.metadata.participants.indexOf(summoner1.puuid);
+          result = result.info.participants[resultIdx];
           summoner1Match.push(result);
         }
       }
@@ -205,15 +209,64 @@ app.get("/search", async (req, res) => {
       }
     }
     await makeSummonerMatch();
-    const data = {
-      id1: req.query.id1,
-      id2: req.query.id2,
-      id3: req.query.id3,
-      id4: req.query.id4,
-      id5: req.query.id5,
-      result: 74,
+
+    const infoArr = ["largestKillingSpree", "goldEarned", "timePlayed", "assists", "deaths", "kills", "detectorWardsPlaced", "killingSprees", "wardsKilled", "wardsPlaced", "visionScore", "totalDamageDealtToChampions", "totalDamageTaken", "totalMinionsKilled", "neutralMinionsKilled"];
+    var player1Idx = 0, player2Idx = 0, player3Idx = 0, player4Idx = 0, player5Idx = 0;
+    for(const match of summoner1Match){
+      for(const info of infoArr){
+        player1Data[player1Idx++] += parseInt(match[info]);
+      }
+    }
+    for(const match of summoner2Match){
+      for(const info of infoArr){
+        player2Data[player2Idx++] += parseInt(match[info]);
+      }
+    }
+    for(const match of summoner3Match){
+      for(const info of infoArr){
+        player3Data[player3Idx++] += parseInt(match[info]);
+      }
+    }
+    for(const match of summoner4Match){
+      for(const info of infoArr){
+        player4Data[player4Idx++] += parseInt(match[info]);
+      }
+    }
+    for(const match of summoner5Match){
+      for(const info of infoArr){
+        player5Data[player5Idx++] += parseInt(match[info]);
+      }
+    }
+
+    for(var i=0; i<infoArr.length; i++){
+      player1Data[i] /= player1Idx;
+      player2Data[i] /= player2Idx;
+      player3Data[i] /= player3Idx;
+      player4Data[i] /= player4Idx;
+      player5Data[i] /= player5Idx;
+    }
+
+    var options = {
+      mode: 'text',
+      pythonPath: '',
+      pythonOptions: ['-u'],
+      scriptPath: '',
+      args: [player1Data, player2Data, player3Data, player4Data, player5Data]
     };
-    res.render("form.ejs", data);
+
+    PythonShell.PythonShell.run('predict.py', options, function (err, results) {
+      if (err) throw err;
+      console.log('results: %j', results);
+      const data = {
+        id1: req.query.id1,
+        id2: req.query.id2,
+        id3: req.query.id3,
+        id4: req.query.id4,
+        id5: req.query.id5,
+        result: results
+      };
+      res.render("form.ejs", data);
+    });
 
   } catch (error) {
     console.log(error);
